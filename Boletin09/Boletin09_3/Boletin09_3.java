8004d55fbbe9357d2780a752a5244521baf9f906
Angel <apineiroandion@danielcastelao.org>
2023-10-30 10:28:24 +0100

    Ejercicio 3 Boletin 9

--- Boletin09/Boletin09_3/Boletin09_3.java
+++ Boletin09/Boletin09_3/Boletin09_3.java
@@ -1,10 +1,13 @@
 
 package boletin09_3;
 
+import javax.swing.JOptionPane;
+
 public class Boletin09_3 {
 
     public static void main(String[] args) {
-        int base=
+        int base=Comprobador.comprobarDatos(LerDatos.lerEnteiro("Introduce la base:"));
+        int altura=Comprobador.comprobarDatos(LerDatos.lerEnteiro("Introduce la altura:"));
+        JOptionPane.showMessageDialog(null, "El area es: "+ base*altura);
     }
-    LerDatos.lerEnteiro("Introduce la base:");
 }
--- Boletin09/Boletin09_3/Comprobador.java
+++ Boletin09/Boletin09_3/Comprobador.java
@@ -2,7 +2,7 @@
 package boletin09_3;
 
 public class Comprobador {
-    public static float comprobarDatos(int num1){
+    public static int comprobarDatos(int num1){
         while(num1<0){
             num1=LerDatos.lerEnteiro("El numero es negativo, introduce otro: ");
         }
