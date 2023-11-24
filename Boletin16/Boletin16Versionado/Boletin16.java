f1b789281f6fe136a2f42585ba9cebf0ca80b004
Angel <apineiroandion@danielcastelao.org>
2023-11-24 12:23:25 +0100

    Verion del boletin 16 con la clase Biblioteca y usando arrys

---
+++ Boletin16/Boletin16Versionado/Boletin16.java
@@ -0,0 +1,13 @@
+
+package boletin16;
+
+import javax.swing.JOptionPane;
+
+public class Boletin16 {
+
+    public static void main(String[] args) {
+        Biblioiteca biblioteca=new Biblioiteca();
+        Menu.pintarMenu(biblioteca);
+    }
+    
+}
